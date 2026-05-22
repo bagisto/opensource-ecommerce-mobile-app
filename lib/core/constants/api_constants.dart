6a8abbdcfc7c0f4cbc84ef1cda6dfa7e9975fea5
SocialMobikul <158288809+SocialMobikul@users.noreply.github.com>
2026-05-22 13:52:50 +0530

    account address refresh and banner crop fixes

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,28 +1,10 @@
-// /// Bagisto API endpoint
-// const String bagistoEndpoint =
-//     'https://your-bagisto-domain.com/graphql';
-
-// /// Storefront key for Bagisto API
-// const String storefrontKey =
-//     'your_storefront_key_here';
-
-// /// Default channel code used by request headers.
-// const String channelCode = 'default';
-
-// /// Default Bagisto channel ID used during app bootstrap.
-// const int channelId = 1;
-
-// /// Company name
-// const String companyName = 'Your Company Name';
-
-
 /// Bagisto API endpoint
 const String bagistoEndpoint =
-    'https://api-demo.bagisto.com/api/graphql';  
+    'https://your-bagisto-domain.com/graphql';
 
 /// Storefront key for Bagisto API
 const String storefrontKey =
- 'pk_storefront_Z10H0ZPzK8f1oj0nN850yiNwuBOjig31';  
+    'your_storefront_key_here';
 
 /// Default channel code used by request headers.
 const String channelCode = 'default';
@@ -31,4 +13,4 @@ const String channelCode = 'default';
 const int channelId = 1;
 
 /// Company name
-const String companyName = 'Webkul Software (Registered in India)';
+const String companyName = 'Your Company Name';
