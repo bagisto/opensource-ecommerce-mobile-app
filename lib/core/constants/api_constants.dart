f66839f9b657bc984f0bb8237aa0ba371531d727
SocialMobikul <158288809+SocialMobikul@users.noreply.github.com>
2026-05-22 15:20:39 +0530

    order and invoice page product more-info action issue fixed

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,10 +1,27 @@
+// /// Bagisto API endpoint
+// const String bagistoEndpoint =
+//     'https://your-bagisto-domain.com/graphql';
+
+// /// Storefront key for Bagisto API
+// const String storefrontKey =
+//     'your_storefront_key_here';
+
+// /// Default channel code used by request headers.
+// const String channelCode = 'default';
+
+// /// Default Bagisto channel ID used during app bootstrap.
+// const int channelId = 1;
+
+// /// Company name
+// const String companyName = 'Your Company Name';
+
 /// Bagisto API endpoint
 const String bagistoEndpoint =
-    'https://your-bagisto-domain.com/graphql';
+    'https://api-demo.bagisto.com/api/graphql';  
 
 /// Storefront key for Bagisto API
 const String storefrontKey =
-    'your_storefront_key_here';
+ 'pk_storefront_Z10H0ZPzK8f1oj0nN850yiNwuBOjig31';  
 
 /// Default channel code used by request headers.
 const String channelCode = 'default';
@@ -13,4 +30,4 @@ const String channelCode = 'default';
 const int channelId = 1;
 
 /// Company name
-const String companyName = 'Your Company Name';
+const String companyName = 'Webkul Software (Registered in India)';
